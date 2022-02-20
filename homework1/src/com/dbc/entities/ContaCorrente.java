bab3f1e778f34ac7949d3376aabb21f6dcbb7bbf
Lucas K. P <lucaskaczyn@gmail.com>
2022-02-19 21:16:10 -0300

    feat: entidade da ContaCorrente

---
+++ homework1/src/com/dbc/entities/ContaCorrente.java
@@ -0,0 +1,67 @@
+package com.dbc.entities;
+
+public class ContaCorrente {
+
+    public Cliente cliente;
+    public String numeroConta;
+    public int agencia;
+    public double saldo;
+    public double chequeEspecial;
+
+    public ContaCorrente(Cliente cliente, String numeroConta, int agencia, double saldo, double chequeEspecial) {
+        this.cliente = cliente;
+        this.numeroConta = numeroConta;
+        this.agencia = agencia;
+        this.saldo = saldo;
+        this.chequeEspecial = chequeEspecial;
+    }
+
+    public void imprimirContaCorrente() {
+        System.out.println(
+                "Nome: " + cliente.nome +
+                "\nCPF: " + cliente.cpf +
+                "\n---------------------------------" +
+                "\nConta: " + numeroConta + " - " + agencia +
+                "\n---------------------------------" +
+                "\nSaldo total: " + retornarSaldoComChequeEspecial()
+        );
+    }
+
+    public boolean sacar(double valor) {
+        if (valor <= retornarSaldoComChequeEspecial() && valor > 0) {
+            double valorSaldoRetirado = valor - chequeEspecial;
+            double valorChequeEspecialRetirado = valor - saldo;
+            saldo -= valorSaldoRetirado;
+            chequeEspecial -= valorChequeEspecialRetirado;
+            return true;
+        } else if (valor <= 0) {
+            System.out.println("Impossível retirar valores negativos ou nulos.");
+        } else {
+            System.out.println("Valor inválido de saque.");
+        }
+        return false;
+    }
+
+    public boolean depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            return true;
+        } else {
+            System.out.println("Impossível depositar valores negativos ou nulos.");
+        }
+        return false;
+    }
+
+    public double retornarSaldoComChequeEspecial() {
+        return saldo + chequeEspecial;
+    }
+
+    public boolean transferir(ContaCorrente conta, double valor) {
+        if (conta.numeroConta != null) {
+            sacar(valor);
+            conta.depositar(valor);
+            return true;
+        }
+        return false;
+    }
+}
