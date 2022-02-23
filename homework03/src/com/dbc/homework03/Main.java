c17fd894b48aca3de0c7352da64a12c6ee6f0bcd
Lucas K. P <lucaskaczyn@gmail.com>
2022-02-22 23:31:47 -0300

    feat: novos testes

--- homework03/src/com/dbc/homework03/Main.java
+++ homework03/src/com/dbc/homework03/Main.java
@@ -2,52 +2,71 @@ package com.dbc.homework03;
 
 import com.dbc.homework03.entities.*;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         Contato contatoLucas = new Contato("Telefone pessoal", "996829690", 1);
         Endereco enderecoLucas = new Endereco(1, "Av. Mauricio Cardoso", 2187, "casa", "96700-000", "São Jerônimo", "RS", "Brasil");
-        Contato[] listaContatosLucas = new Contato[2];
-        Endereco[] listaEnderecosLucas = new Endereco[2];
-        listaContatosLucas[0] = contatoLucas;
-        listaEnderecosLucas[0] = enderecoLucas;
+        ArrayList<Contato> listaContatosLucas = new ArrayList<>();
+        ArrayList<Endereco> listaEnderecosLucas = new ArrayList<>();
+        listaContatosLucas.add(contatoLucas);
+        listaEnderecosLucas.add(enderecoLucas);
         Cliente lucas = new Cliente("Lucas Kaczynski Primon", "054.108.160-80", listaContatosLucas, listaEnderecosLucas);
-        ContaCorrente contaCorrenteLucas = new ContaCorrente(lucas, "2189", "5", 2500.5, 30);
-        lucas.imprimirCliente();
-
-        System.out.println("\n -------------- Testes com a Conta Corrente -------------------");
-
-        contaCorrenteLucas.imprimir();
-        contaCorrenteLucas.depositar(40);
-        System.out.println("\nSaldo total depois do deposito: " + contaCorrenteLucas.retornarSaldoComChequeEspecial());
-        contaCorrenteLucas.sacar(1000);
-        System.out.println("\nSaldo total depois do saque: " + contaCorrenteLucas.retornarSaldoComChequeEspecial());
 
         Contato contatoNicolas = new Contato("Telefone pessoal", "XXXXXXXXXXXXX", 1);
         Endereco enderecoNicolas = new Endereco(1, "XX XXXXXXX XXXXX", 200, "casa", "9670X-XXX", "Charqueadas", "RS", "Brasil");
-        Contato[] listaContatosNicolas = new Contato[2];
-        Endereco[] listaEnderecosNicolas = new Endereco[2];
-        listaContatosNicolas[0] = contatoNicolas;
-        listaEnderecosNicolas[0] = enderecoNicolas;
+        ArrayList<Contato> listaContatosNicolas = new ArrayList<>();
+        ArrayList<Endereco> listaEnderecosNicolas = new ArrayList<>();
+        listaContatosNicolas.add(contatoNicolas);
+        listaEnderecosNicolas.add(enderecoNicolas);
         Cliente nicolas = new Cliente("Nicolas Fiedler", "XXX.XXX.XXX-XX", listaContatosNicolas, listaEnderecosNicolas);
-        ContaCorrente contaCorrenteNicolas = new ContaCorrente(nicolas, "3000", "7", 3000, 0);
 
-        contaCorrenteNicolas.imprimir();
+        ContaCorrente contaCorrenteLucas = new ContaCorrente(lucas, "2189", "5", 2500.5, 30);
+        ContaPoupanca contaPoupancaLucas = new ContaPoupanca(lucas, "3021", "1", 1800);
+        ContaPagamento contaPagamentoNicolas = new ContaPagamento(nicolas, "3000", "7", 1100);
 
-        contaCorrenteLucas.transferir(contaCorrenteNicolas, 254.70);
-        System.out.println("\nSaldo total da conta do Lucas depois que transferiu: " + contaCorrenteLucas.retornarSaldoComChequeEspecial());
 
-        contaCorrenteNicolas.imprimir();
+        System.out.println("----------------------- Mostrando Cliente -----------------------");
+        lucas.imprimirCliente();
 
-        System.out.println("\n -------------- Testes com a Conta Poupança -------------------");
+        System.out.println("----------------------- Testes Conta Corrente -----------------------");
+        contaCorrenteLucas.imprimir();
+        contaCorrenteLucas.depositar(200);
+        System.out.println("\n--------- Após depósito");
+        contaCorrenteLucas.imprimir();
 
-        ContaPoupanca contaPoupancaLucas = new ContaPoupanca(lucas, "6780542", "05", 1345.00);
+        contaCorrenteLucas.transferir(contaPoupancaLucas, 240.75);
+        System.out.println("\n--------- Após transeferencia para outra conta");
+        contaCorrenteLucas.imprimir();
+
+        System.out.println("\n----------------------- Testes Conta Corrente -----------------------");
+        contaPoupancaLucas.imprimir();
+
+        contaPoupancaLucas.sacar(100);
+        System.out.println("\n--------- Após sacar");
+        contaPoupancaLucas.imprimir();
+
         contaPoupancaLucas.creditarTaxa();
+        System.out.println("\n--------- Após creditar taxa");
         contaPoupancaLucas.imprimir();
 
-        contaCorrenteNicolas.transferir(contaPoupancaLucas, 300);
-        System.out.println("\nSaldo total da conta corrente do Nicolas: " + contaCorrenteNicolas.retornarSaldoComChequeEspecial());
-        contaPoupancaLucas.imprimir();
+        System.out.println("\n----------------------- Testes Conta Pagamento -----------------------");
+        contaPagamentoNicolas.imprimir();
+
+        contaPagamentoNicolas.sacar(1096);
+        System.out.println("\n--------- Após tentar sacar valor inválido");
+        contaPagamentoNicolas.imprimir();
+
+        contaPagamentoNicolas.transferir(contaCorrenteLucas, 1);
+        System.out.println("\n--------- Após transefir");
+        contaPagamentoNicolas.imprimir();
+
+        contaPagamentoNicolas.sacar(1094.75);
+        System.out.println("\n--------- Após sacar valor máximo que retire tudo, considerando taxa de saque");
+        contaPagamentoNicolas.imprimir();
+
     }
 }
 
