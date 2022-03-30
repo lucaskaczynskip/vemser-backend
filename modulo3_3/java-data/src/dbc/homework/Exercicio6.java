3bcf6205f893f9d023163cd77ca6991fd4f339c4
lucaskaczynskip <lucaskaczyn@gmail.com>
2022-03-30 08:17:34 -0300

    corrigido o calculo de tempo

--- modulo3_3/java-data/src/dbc/homework/Exercicio6.java
+++ modulo3_3/java-data/src/dbc/homework/Exercicio6.java
@@ -1,32 +1,29 @@
 package dbc.homework;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
 
 public class Exercicio6 {
 	public static void main(String[] args) {
 		
-		// data do show
-		LocalDateTime dataShow = LocalDateTime.of(2024, 9, 14, 18, 30);
-		
-		// vou considerar os fusos para definir a hora
-		ZoneId zoneId = ZoneId.of("UTC+01:00");
-	    ZonedDateTime zonedDateTimeEuropa = ZonedDateTime.of(dataShow, zoneId);
-	   
-	    LocalDateTime hojeBrasil = LocalDateTime.now();
-	    ZoneId zoneIdBr = ZoneId.of("UTC-03:00");
-	    ZonedDateTime zonedDateTimeBrasil = ZonedDateTime.of(hojeBrasil, zoneIdBr);
-	    
-	    System.out.println("Para o show faltam: ");
-	    System.out.println("Dias: " + ChronoUnit.DAYS.between(zonedDateTimeBrasil, zonedDateTimeEuropa));
-	    System.out.println("Meses: " + ChronoUnit.MONTHS.between(zonedDateTimeBrasil, zonedDateTimeEuropa));
-	    System.out.println("Anos: " + ChronoUnit.YEARS.between(zonedDateTimeBrasil, zonedDateTimeEuropa));
-	    System.out.println("Horas: " + ChronoUnit.HOURS.between(zonedDateTimeBrasil, zonedDateTimeEuropa));
-	    System.out.println("Minutos: " + ChronoUnit.MINUTES.between(zonedDateTimeBrasil, zonedDateTimeEuropa));
-	    System.out.println("Segundos: " + ChronoUnit.SECONDS.between(zonedDateTimeBrasil, zonedDateTimeEuropa));
-	    
+		ZonedDateTime dateTimeBrasil = LocalDateTime.now().atZone(ZoneId.of("Brazil/East"));
+        ZonedDateTime dateTimeEuropa = LocalDateTime.of(2024, 9, 14, 18, 30).atZone(ZoneId.of("Europe/London"));
+        
+        long difOffset = dateTimeEuropa.getOffset().getTotalSeconds() - dateTimeBrasil.getOffset().getTotalSeconds();
+        
+        Period periodo = Period.between(dateTimeBrasil.toLocalDate(), dateTimeEuropa.toLocalDate());
+        
+        Duration duracao = Duration.between(dateTimeEuropa.minusSeconds(difOffset).toLocalTime(), dateTimeBrasil.toLocalTime());
+        
+        long hour = duracao.abs().toHours();
+        long minutes = duracao.abs().toMinutes() % 60;
+        long seconds = duracao.abs().getSeconds() % 60;
+        
+        System.out.printf("Faltam %d anos, %d meses, %d dias, %d horas, %d minutos, %d segundos para o show!",
+        		periodo.getYears(), periodo.getMonths(), periodo.getDays(), hour, minutes, seconds);
 	}
 	
 }
